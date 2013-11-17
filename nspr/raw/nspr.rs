5dc3842224e3893b9d531b50f19faead49c005aa
Michael Letterle <michael.letterle@gmail.com>
2013-11-17 13:45:44 -0500

    Make 'em pub

--- nspr/raw/nspr.rs
+++ nspr/raw/nspr.rs
@@ -22,14 +22,14 @@ pub struct PRNetAddr {
 
 }
 extern "C" {  
-fn PR_OpenTCPSocket(af: c_ushort) -> *c_void;
-fn PR_Connect(fd: *c_void, addr: *PRNetAddr, timout: c_uint) -> PRStatus;
-fn PR_Close(fd: *c_void) -> PRStatus;
-fn PR_StringToNetAddr(string: *c_char, addr: *c_void) -> PRStatus;
-fn PR_GetError() -> c_int;
-fn PR_ErrorToName(error: c_int) -> *c_char;
-fn PR_htons(conversion: c_ushort) -> c_ushort;
-fn PR_htonl(conversion: c_uint) -> c_uint;
-fn PR_Write(fd: *c_void, buf: *c_void, amount: c_int) -> c_int;
-fn PR_Read(fd: *c_void, buf: *c_void, amount: c_int) -> c_int;
+pub fn PR_OpenTCPSocket(af: c_ushort) -> *c_void;
+pub fn PR_Connect(fd: *c_void, addr: *PRNetAddr, timout: c_uint) -> PRStatus;
+pub fn PR_Close(fd: *c_void) -> PRStatus;
+pub fn PR_StringToNetAddr(string: *c_char, addr: *c_void) -> PRStatus;
+pub fn PR_GetError() -> c_int;
+pub fn PR_ErrorToName(error: c_int) -> *c_char;
+pub fn PR_htons(conversion: c_ushort) -> c_ushort;
+pub fn PR_htonl(conversion: c_uint) -> c_uint;
+pub fn PR_Write(fd: *c_void, buf: *c_void, amount: c_int) -> c_int;
+pub fn PR_Read(fd: *c_void, buf: *c_void, amount: c_int) -> c_int;
 }
